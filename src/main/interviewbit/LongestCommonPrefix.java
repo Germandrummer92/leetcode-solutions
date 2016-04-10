8cac278cf6205e49e6adba094eed9baee479ad5d
Daniel Draper <Germandrummer92@users.noreply.github.com>
2016-04-11 00:14:33 +0200

    Create LongestCommonPrefix.java
    New Solution Interviewbit

---
+++ src/main/interviewbit/LongestCommonPrefix.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+	public String longestCommonPrefix(ArrayList<String> a) {
+	        String curLongest = "";
+	        String curString = a.get(0);
+	        for (int j = 1; j <= curString.length(); j++) {
+	            String curPrefix = a.get(0).substring(0, j);
+	            boolean foundFalse = false;
+	            for (int k = 1; k < a.size() && !foundFalse; k++) {
+	                String toCompare = a.get(k);
+	                if (toCompare.length() < j || !curPrefix.equals(toCompare.substring(0, j))) {
+	                    foundFalse = true;
+	                }
+	            }
+	            if (!foundFalse) {
+	                curLongest = curPrefix;
+	            }
+	    }
+	    return curLongest;
+	}
+}
